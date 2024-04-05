85951590f1faffccc2f774705e2fe5ad2dafe984
Mohammed-Mujib <wadmujib@gmail.com>
2024-04-05 14:18:22 +0200

    finish math 2

--- js/cal.js
+++ js/cal.js
@@ -2,23 +2,41 @@ let display = document.getElementById("display");
 let equal = document.getElementById("equal");
 let clear = document.getElementById("clear");
 let ele = document.querySelectorAll(".form .bt");
+let del = document.getElementById("delete");
 let display_record = [];
 let record = [];
 let result = document.getElementById("equal");
-clear.onclick = function(){
-  display.innerHTML = "";
+clear.onclick = function () {
+  display.innerHTML = null;
+  display_record = [];
   record = [];
-}
-result.onclick = function(){
-  if (record.length === 0) {
+};
+result.onclick = function () {
+  if (record.length < 1) {
     display.innerHTML = "0";
+  } else {
+    display.innerHTML = eval(record.join(""));
+    display_record = [`${display.innerHTML}`];
+    record = [display.innerHTML]
   }
-  display.innerHTML= eval(record.join(""));
-}
+};
 ele.forEach(function (e) {
   e.addEventListener("click", function () {
-    display.innerHTML += this.innerHTML;
-    record.push(this.dataset.val)
-    console.log(record);
-  })
+    let arr = ["+", "-", "/", "*"];
+    if (arr.includes(`${record.slice(-1,)}`) &&arr.includes(`${this.dataset.val}`) ) {
+      record[-1] = this.dataset.val;
+      display_record[-1] = `${this.innerHTML}`;
+      display.innerHTML = display_record.join("");
+    } else {
+      display_record.push(`${this.innerHTML}`);
+      display.innerHTML = display_record.join("");
+      record.push(this.dataset.val);
+    }
+  });
 });
+del.onclick = function () {
+  display_record.pop();
+  display.innerHTML = display_record.join("");
+  record.pop();
+  console.log(record);
+};
